4a5ef81acad501e92ebfb8c4f30175ecb9459f19
Obsidian <josephineyzc@gmail.com>
2025-03-26 16:22:19 -0400

    Cleaned up project pages

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+'use strict';
 
+// window.addEventListener('scroll',function() {
+//     const header = document.querySelector('header');
+//     if (window.scrollY > 50) {
+//         header.classList.add('hidden');
+//     } else {
+//         header.classList.remove('hidden');
+//     }
+// })
+
+// window.addEventListener('hover',function() {
+
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
